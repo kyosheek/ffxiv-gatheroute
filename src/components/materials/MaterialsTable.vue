eb92b6e4eb0fb0aed1d9e2cc8a9c2e23d3b0eb13
kyoshee <dsuchkov511@gmail.com>
2022-10-27 19:17:20 +0600

    render table, move from css to scss

---
+++ src/components/materials/MaterialsTable.vue
@@ -0,0 +1,63 @@
+<script>
+    import { tsvParse } from "d3-dsv";
+
+    import Table from "../ui/Table.vue";
+
+    export default {
+        name: 'MaterialsTable',
+        components: { Table },
+        data() {
+            return {
+                isLoading: {
+                    type: Boolean,
+                    default: false,
+                },
+                rows: {
+                    type: Array,
+                    default: []
+                },
+                columns: {
+                    type: Array,
+                    default: []
+                },
+                err: {
+                    type: String,
+                    default: null
+                }
+            };
+        },
+        created()
+        {
+            fetch('assets/data/materials.tsv')
+                .then(response => response.text())
+                .then(blob => tsvParse(blob))
+                .then(data =>
+                {
+                    this.rows = [...data];
+                    this.columns = data.columns;
+                })
+                .catch(err =>
+                {
+                    this.err = JSON.stringify(err, null, 2);
+                })
+                .finally(() =>
+                {
+                    this.isLoading = false;
+                });
+        }
+    }
+</script>
+
+<template>
+    <section>
+        <h2>Materials table</h2>
+        <div class="table-wrap" :class="{ 'is-loading': isLoading }">
+            <Table :columns="columns"
+                   :rows="rows" />
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ui/Table.vue
@@ -0,0 +1,44 @@
+<script>
+    export default {
+        name: 'Table',
+        props: {
+            columns: {
+                type: Array,
+                default() { return []; }
+            },
+            rows: {
+                type: Array,
+                default() { return []; }
+            }
+        },
+        computed: {
+            hasColumns() { return this.columns.length > 0; },
+            hasRows() { return this.rows.length > 0; }
+        }
+    }
+</script>
+
+<template>
+    <table>
+        <thead v-if="hasColumns">
+            <tr>
+                <template v-for="column in columns">
+                    <th>{{ column }}</th>
+                </template>
+            </tr>
+        </thead>
+        <tbody v-if="hasRows">
+            <template v-for="row in rows">
+                <tr>
+                    <template v-for="column in columns">
+                        <td>{{ row[column] }}</td>
+                    </template>
+                </tr>
+            </template>
+        </tbody>
+    </table>
+</template>
+
+<style lang="scss">
+
+</style>
