7e2c699d6b2508c7fcb612376bc3646c49aae5cf
kyoshee <dsuchkov511@gmail.com>
2022-12-12 17:44:50 +0600

    update materials table, remove data link from footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -7,23 +7,27 @@
         <article>
             <h3>Contribute</h3>
             <ul class="ul_inline">
-                <li><a href="https://github.com/kyosheek/ffxiv-gatheroute-data">Data GitHub Repository</a></li>
-                <li><a href="https://github.com/kyosheek/ffxiv-gatheroute-data">Client GitHub Repository</a></li>
+                <li><a href="https://github.com/kyosheek/ffxiv-gatheroute"
+                       target="_blank">GitHub Repository</a></li>
             </ul>
         </article>
         <article>
             <h3>Connect</h3>
             <ul class="ul_inline">
-                <li><a href="https://discordapp.com/users/190847929433587712">Discord</a></li>
-                <li><a href="http://kyoshee.xyz">Personal Website</a></li>
+                <li><a href="https://discordapp.com/users/190847929433587712"
+                       target="_blank">Discord</a></li>
+                <li><a href="http://kyoshee.xyz"
+                       target="_blank">Personal Website</a></li>
             </ul>
         </article>
 
         <article>
             <h3>Support</h3>
             <ul class="ul_inline">
-                <li><a href="https://paypal.me/kyoshee">Paypal</a></li>
-                <li><a href="https://ko-fi.com/kyoshee">Ko-fi</a></li>
+                <li><a href="https://paypal.me/kyoshee"
+                       target="_blank">Paypal</a></li>
+                <li><a href="https://ko-fi.com/kyoshee"
+                       target="_blank">Ko-fi</a></li>
             </ul>
         </article>
 
