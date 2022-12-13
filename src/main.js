ea1ca8c8c8954565ad7dde2fd38197b5e939cd07
kyoshee <dsuchkov511@gmail.com>
2022-12-13 22:37:13 +0600

    add vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.scss';
+import App from './App.vue';
+
+import { inject } from '@vercel/analytics';
 
 createApp(App).mount('#app')
+
+inject();
